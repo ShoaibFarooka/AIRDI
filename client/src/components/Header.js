49c494a51b087c65ae7486b04d8161fee3a57fa8
Shoaib Farooka <shoaibfarooka@gmail.com>
2024-05-05 03:16:15 +0500

    email template change

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -76,30 +76,36 @@ const Header = () => {
                     }
                 </div>
             </div>
-            <div className="title">
-                <div className="top-row">
-                    <div className="text">NYC</div>
-                    <div className="icon"><PiArrowsLeftRightBold /></div>
-                    <div className="text">AIRPORT</div>
-                </div>
-                <div className="text-2">Book Your Trip For</div>
-                <div className="schedule-row">
-                    <div className="para">Fordham</div>
-                    <PiArrowsLeftRightBold size={24} className="direction-icon" />
-                    <div className="para">JFK: </div>
-                    <div className="para">June 14 - June 30 (Fri - Mon)</div>
-                </div>
-                <div className="schedule-row">
-                    <div className="para">Fordham</div>
-                    <PiArrowsLeftRightBold size={24} className="direction-icon" />
-                    <div className="para">JFK: </div>
-                    <div className="para">July 01 - July 31</div>
-                </div>
-                <div className="schedule-row">
-                    <div className="para">Fordham</div>
-                    <PiArrowsLeftRightBold size={24} className="direction-icon" />
-                    <div className="para">LGA: </div>
-                    <div className="para">July 01 - July 31</div>
+            <div className="title-wrapper">
+                <div className="title">
+                    <div className="top-row">
+                        <div className="text">NYC</div>
+                        <div className="icon"><PiArrowsLeftRightBold /></div>
+                        <div className="text">AIRPORT</div>
+                    </div>
+                    {window.location.pathname === '/' &&
+                        <div className="">
+                            <div className="text-2">Book Your Trip For</div>
+                            <div className="schedule-row">
+                                <div className="para">Fordham</div>
+                                <PiArrowsLeftRightBold size={24} className="direction-icon" />
+                                <div className="para">JFK: </div>
+                                <div className="para">June 14 - June 30 (Fri - Mon)</div>
+                            </div>
+                            <div className="schedule-row">
+                                <div className="para">Fordham</div>
+                                <PiArrowsLeftRightBold size={24} className="direction-icon" />
+                                <div className="para">JFK: </div>
+                                <div className="para">July 01 - July 31</div>
+                            </div>
+                            <div className="schedule-row">
+                                <div className="para">Fordham</div>
+                                <PiArrowsLeftRightBold size={24} className="direction-icon" />
+                                <div className="para">LGA: </div>
+                                <div className="para">July 01 - July 31</div>
+                            </div>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
